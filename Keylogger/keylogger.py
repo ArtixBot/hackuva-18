33c8b4eb2c1018bdbe443c423cc200cc3f984220
Dennis Yang <dhy4wb@virginia.edu>
2018-03-25 01:51:35 -0400

    I am frustrate
    help

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -1,108 +1,119 @@
 import csv
 import re
 
-#In this file we are reading in 2 different files:
-#File 1: Database file full of private info from the user
-#File 2: File produced from keylogger
-
-
-
-# #date = {"SSN" : "xxx-xx-xxxx", "email" : "xxxx@.edu", "phone" : "xxx-xxx-xxxx" }
-# privateInfo = []
-# #privateSSN
-# #privateEmail
-# #privatePhone
-# dataInput = open('database.txt', 'r')
-# pSSN = []
-# pEmail = []
+# #In this file we are reading in 2 different files:
+# #File 1: Database file full of private info from the user
+# #File 2: File produced from keylogger
 #
-# data = dataInput.readlines()
+#
+#
+# # #date = {"SSN" : "xxx-xx-xxxx", "email" : "xxxx@.edu", "phone" : "xxx-xxx-xxxx" }
+# # privateInfo = []
+# # #privateSSN
+# # #privateEmail
+# # #privatePhone
+# # dataInput = open('database.txt', 'r')
+# # pSSN = []
+# # pEmail = []
+# #
+# # data = dataInput.readlines()
+# # for element in data:
+# #     print(type(element), type(data))
+# #     strippedElement = element.strip("\n")
+# #     miniInfo = strippedElement.split(",")
+# #     stream.append(miniInfo)
+# #     print(stream)
+# #
+# # #key,date,time
+# datafile = open('database.txt', 'r')
+# # print(list(datafile))
+# infoPart = []
+# for element in datafile:
+#     #print(element)
+#     strippedElement = element.strip("\n")
+#     miniInfo = strippedElement.split(" ")
+#     #print(miniInfo)
+#     infoPart.append(miniInfo)
+# #print(infoPart)
+#
+#
+#
+# myfile = open('log.txt', 'r')
+# stream = []
+# data = myfile.readlines()
 # for element in data:
-#     print(type(element), type(data))
+#     #print(type(element), type(data))
 #     strippedElement = element.strip("\n")
 #     miniInfo = strippedElement.split(",")
 #     stream.append(miniInfo)
-#     print(stream)
+# print(stream)
 #
-# #key,date,time
-datafile = open('database.txt', 'r')
-# print(list(datafile))
-infoPart = []
-for element in datafile:
-    #print(element)
-    strippedElement = element.strip("\n")
-    miniInfo = strippedElement.split(" ")
-    #print(miniInfo)
-    infoPart.append(miniInfo)
-#print(infoPart)
-
-
-
-myfile = open('log.txt', 'r')
-stream = []
-data = myfile.readlines()
-for element in data:
-    #print(type(element), type(data))
-    strippedElement = element.strip("\n")
-    miniInfo = strippedElement.split(",")
-    stream.append(miniInfo)
-print(stream)
-
-
-def findOccurSSN(stream):
-    listofSSN = infoPart[0]
-    retIndex=[]
-    for element in listofSSN:
-        retIndex = [m.start() for m in re.finditer(element, stream)]
-
-    return retIndex
-
-def findOccurEmail(stream):
-    listofEmail = infoPart[1]
-    retIndex=[]
-    for element in listofEmail:
-        retIndex = [m.start() for m in re.finditer(element, stream)]
-
-    return retIndex
-
-def toSpace(x):
-    if x == 'Key.space':
-        return ' '
-    return x
-
-charOnlyStr = ""
-def checkInfo(stream):
-    stream = [list(map(lambda y: toSpace(y), l)) for l in stream]
-    #print(list(stream))
-    charOnly = list(map(lambda x: x[0], stream))
-    charOnlyStr = "".join(map(str,charOnly))
-
-    #print(charOnly)
-    #return(charOnlyStr)
-    SSNIndex = findOccurSSN(charOnlyStr)
-    print(SSNIndex)
-    EmailIndex = findOccurEmail(charOnlyStr)
-    file = open("matches.txt", 'w')
-    for index in SSNIndex:
-        file.write(infoPart[index])
-    for index in EmailIndex:
-        file.write(infoPart[index])
-    file.close()
-
-
-
-
-checkInfo(stream)
-
-#checkInfo(stream)
+#
+# def findOccurSSN(stream):
+#     listofSSN = infoPart[0]
+#     retIndex=[]
+#     for element in listofSSN:
+#         retIndex = [m.start() for m in re.finditer(element, stream)]
+#
+#     return retIndex
+#
+# def findOccurEmail(stream):
+#     listofEmail = infoPart[1]
+#     retIndex=[]
+#     for element in listofEmail:
+#         retIndex = [m.start() for m in re.finditer(element, stream)]
+#
+#     return retIndex
+#
+# def toSpace(x):
+#     if x == 'Key.space':
+#         return ' '
+#     return x
+#
+# charOnlyStr = ""
+# def checkInfo(stream):
+#     stream = [list(map(lambda y: toSpace(y), l)) for l in stream]
+#     #print(list(stream))
+#     charOnly = list(map(lambda x: x[0], stream))
+#     charOnlyStr = "".join(map(str,charOnly))
+#
+#     #print(charOnly)
+#     #return(charOnlyStr)
+#     SSNIndex = findOccurSSN(charOnlyStr)
+#     print(SSNIndex)
+#     EmailIndex = findOccurEmail(charOnlyStr)
+#     file = open("matches.txt", 'w')
+#     for index in SSNIndex:
+#         file.write(infoPart[index])
+#     for index in EmailIndex:
+#         file.write(infoPart[index])
+#     file.close()
+#
+#
+#
+#
+# checkInfo(stream)
+#
+# #checkInfo(stream)
 
 
 #     for
 #     print(dates)
-testInput =[[123,456], ["cow", "cattle"]]
-enterString = "zxcocow"
+testInput =[[123,456], ["cow@gmail.com", "cattle@yahoo.com", "cowt@gmail.com"]]
+enterString = "zxcocow@gmail.com"
 
 s= testInput[0]
 e= testInput[1]
-for element in enterString:
+print(e)
+# param string=""
+# get letter
+# param counter=0
+# while true:
+#   stream=string+input letter
+#   for char in range(counter,len(stream)):
+#
+fruit = re.compile(r'(?:%s)' % '|'.join(e))
+result = fruit.search(enterString)
+print(result)
+# for element in enterString:
     
